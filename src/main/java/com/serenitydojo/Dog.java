e9c84e65252e444161b8e2396390e6ee0d0d531a
tudiapason <40805731+tudiapason@users.noreply.github.com>
2023-03-30 13:49:22 +0200

    Exercise: Fields and methods

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,52 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private String favoriteToy;
+    private int age;
+    private static String noise = "Woof";
+    private Boolean isFed = false;
+
+    public Dog(String name, String favoriteToy, int age){
+        this.name = name;
+        this.favoriteToy = favoriteToy;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String makeNoise() {
+        return noise;
+    }
+
+    public void feed() {
+        isFed = true;
+    }
+
+    public boolean isFed() {
+        return isFed;
+    }
+}
